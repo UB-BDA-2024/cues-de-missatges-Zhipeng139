b104d5e4e45c183141152560fb9a6c0db348ca1b
Sure <zhipeng139@gmail.com>
2024-05-16 00:51:52 +0200

    multiple consumer

--- consumer/start.sh
+++ consumer/start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# Start the first consumer
-python /app/consumer/main.py /app/consumer/config1.json 
+# Start the first consumer in the background
+python /app/consumer/main.py /app/consumer/config1.json &
 
-# Start the second consumer
-python /app/consumer/main.py /app/consumer/config2.json
+# Start the second consumer in the background
+python /app/consumer/main.py /app/consumer/config2.json &
 
 # Wait for all background processes to complete
 wait
